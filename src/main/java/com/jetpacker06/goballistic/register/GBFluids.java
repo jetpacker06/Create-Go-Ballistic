731d2baec945eed43bc512afb7499a6d974a9050
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2023-08-02 10:37:12 -0500

    Fluid texture

--- src/main/java/com/jetpacker06/goballistic/register/GBFluids.java
+++ src/main/java/com/jetpacker06/goballistic/register/GBFluids.java
@@ -1,19 +1,19 @@
 package com.jetpacker06.goballistic.register;
 
+import com.jetpacker06.goballistic.GoBallistic;
 import com.simibubi.create.AllTags;
 import com.simibubi.create.content.fluids.VirtualFluid;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.FluidEntry;
-import net.minecraft.resources.ResourceLocation;
 
 public class GBFluids {
-    public static ResourceLocation waterStill = new ResourceLocation("block/water_still");
-    public static ResourceLocation waterFlow = new ResourceLocation("block/water_flow");
+  //  public static ResourceLocation waterStill = new ResourceLocation("block/water_still");
+  //  public static ResourceLocation waterFlow = new ResourceLocation("block/water_flow");
 
     public static FluidEntry<VirtualFluid> GUNPOWDER;
 
     public static void registerFluids(CreateRegistrate registrate) {
-        GUNPOWDER = registrate.virtualFluid("gunpowder", waterStill, waterFlow)
+        GUNPOWDER = registrate.virtualFluid("gunpowder", GoBallistic.resource("fluid/gunpowder_still"), GoBallistic.resource("fluid/gunpowder_flow"))
                 .lang("Gunpowder")
                 .tag(AllTags.forgeFluidTag("gunpowder"))
                 .register();
